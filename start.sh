b6e9456a55bc7ff9dcd3022a0a37caffde43ab4f
tunasmoothie <tunasmoothie@gmail.com>
2022-11-01 15:55:44 +0800

    added start.sh

---
+++ start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+BASEDIR=$(dirname "$0")
+
+while [ 1 ]; do
+
+	echo "1: "
+	echo "2: "
+	echo "3: Clean and remove all build products"
+	echo "4: Exit"
+	read ans
+
+	case ${ans} in
+		1) 
+		;;
+		2)
+		;;
+		3)
+		;;
+		4)
+			exit
+		;;
+	esac 
+done
